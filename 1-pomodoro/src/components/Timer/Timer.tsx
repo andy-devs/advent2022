9d6ee5f207214052d2f4f17ab88cf4047c2e2cfd
Andrey Markushin <markushin.andrew@gmail.com>
2022-12-25 19:37:00 +0600

    feat: timer countdown, inputs layout

--- 1-pomodoro/src/components/Timer/Timer.tsx
+++ 1-pomodoro/src/components/Timer/Timer.tsx
@@ -1,9 +1,16 @@
 import dayjs from 'dayjs';
-import useTimer from '../../hooks/useTimer';
+import useTimer from 'hooks/useTimer';
 import { useState } from 'react';
-import { ReactComponent as Gear } from '../../assets/images/gear.svg';
+import { ReactComponent as Gear } from 'assets/images/gear.svg';
+import { ReactComponent as Check } from 'assets/images/check.svg';
+import { useForm } from 'react-hook-form';
 
 interface TimerProps {}
+
+interface TimerInput {
+	minutes: number;
+	seconds: number;
+}
 
 const Timer: React.FC<TimerProps> = () => {
 	const {
@@ -14,19 +21,54 @@ const Timer: React.FC<TimerProps> = () => {
 		isEditing,
 		toggleEdit,
 		setTime,
-	} = useTimer();
+	} = useTimer(15, 0);
+
+	const { register } = useForm<TimerInput>({
+		defaultValues: {
+			minutes: Number(minutes),
+			seconds: Number(seconds),
+		},
+	});
 
 	return (
 		<div className='bg-black rounded-[50%] w-full max-w-[518px] min-h-full max-h-[518px] aspect-[1/1] flex items-center justify-center p-2 shadow-[5px_-16px_50px_rgba(255,255,255,0.15)]'>
 			<div className='w-full h-full bg-background rounded-[50%] flex flex-col items-center justify-center shadow-[inset_0px_0px_114px_rgba(0,0,0,0.45)]'>
-				<span className='font-timer sm:mt-[5%] text-white text-[120px] sm:text-[196px] leading-none text-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'>
-					15:00
-				</span>
-				<button className='font-button text-white tracking-[0.6em] mt-3 sm:mt-6 py-2 pl-4 pr-2 text-center'>
-					START
-				</button>
-				<span className='mt-4 cursor-pointer' onClick={toggleEdit}>
-					<Gear />
+				<div className='font-timer text-white text-[120px] sm:text-[196px] leading-none text-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'>
+					{isEditing ? (
+						<form className='max-w-[304px] flex'>
+							<input
+								className='bg-transparent grow-1 max-w-[160px] focus:outline-none'
+								type='number'
+								placeholder='00'
+								step='1'
+								min='0'
+								max='99'
+								{...register('minutes', { maxLength: 2 })}
+							/>
+							<span>:</span>
+							<input
+								className='bg-transparent grow-1 max-w-[160px] focus:outline-none'
+								type='number'
+								placeholder='00'
+								step='1'
+								min='0'
+								max='99'
+								{...register('seconds', { maxLength: 2 })}
+							/>
+						</form>
+					) : (
+						<span className=''>{`${minutes}:${seconds}`}</span>
+					)}
+				</div>
+				{!isEditing && (
+					<button
+						onClick={toggleActive}
+						className='font-button text-white tracking-[0.6em] mt-3 sm:mt-6 py-2 pl-4 pr-2 text-center'>
+						{isActive ? 'STOP' : 'START'}
+					</button>
+				)}
+				<span onClick={toggleEdit} className='mt-4 cursor-pointer'>
+					{isEditing ? <Check /> : <Gear />}
 				</span>
 			</div>
 		</div>
--- 1-pomodoro/src/hooks/useTimer.ts
+++ 1-pomodoro/src/hooks/useTimer.ts
@@ -1,10 +1,34 @@
 import dayjs from 'dayjs';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const useTimer = (initialTime?: number) => {
-	const [count, setCount] = useState(initialTime);
+const useTimer = (initialMinutes?: number, initialSeconds?: number) => {
+	const getInitialState = () => {
+		if (initialMinutes && initialSeconds) {
+			return initialMinutes * 60 + initialSeconds;
+		} else if (initialMinutes && !initialSeconds) {
+			return initialMinutes * 60;
+		} else if (!initialMinutes && initialSeconds) {
+			return initialSeconds;
+		} else return 15 * 60;
+	};
+
+	const [count, setCount] = useState(() => getInitialState());
 	const [isEditing, setIsEditing] = useState(false);
 	const [isActive, setIsActive] = useState(false);
+
+	useEffect(() => {
+		let interval: ReturnType<typeof setInterval>;
+
+		if (isActive) {
+			interval = setInterval(() => {
+				setCount((prev) => prev - 1);
+			}, 1000);
+		}
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [isActive]);
 
 	const toggleEdit = () => {
 		setIsEditing((prev) => !prev);
@@ -14,14 +38,17 @@ const useTimer = (initialTime?: number) => {
 		setIsActive((prev) => !prev);
 	};
 
-	const setTime = () => {};
+	const setTime = (milliseconds: number) => {};
 
-	const minutes = 15;
-	const seconds = 0;
+	const minutes = Math.floor(count / 60);
+	const seconds = count - minutes * 60;
+
+	const parsedMinutes = minutes < 10 ? '0' + minutes : minutes;
+	const parsedSeconds = seconds < 10 ? '0' + seconds : seconds;
 
 	return {
-		minutes,
-		seconds,
+		minutes: parsedMinutes,
+		seconds: parsedSeconds,
 		isActive,
 		toggleActive,
 		isEditing,
